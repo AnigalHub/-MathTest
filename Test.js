d4f1055786ed559d7e37fcd836590fd24af519c0
AnigalHub <anigal1997@gmail.com>
2021-03-14 22:00:40 +0300

    Класс-Тест, с методом создания теста

---
+++ Test.js
@@ -0,0 +1,24 @@
+const RandomElementArray = require("./RandomElementsArray");
+class Test { //Класс - Тест
+    constructor(number1,number2,answer1,answer2) {
+        this.ArrayAnswer = [];   //пустой массив
+        this.sum = 0;            // сумма чисел
+        this.number1 = number1;   // первое число для подсчета/вопроса
+        this.number2 = number2;   // второе число для подсчета/вопроса
+        this.answer1 = answer1;   // первый ответ для вывода
+        this.answer2 = answer2;   // второй ответ для вывода
+    }
+    CreateTest(){
+        document.getElementById("question").innerHTML = this.number1 + "+" + this.number2 + "=  ?"; // выводим вопрос
+        this.sum = this.number1+this.number2;                                 // считаем сумму чисел
+        this.ArrayAnswer.push(this.sum,this.answer1,this.answer2);            // заполняем массив
+        RandomElementArray(this.ArrayAnswer);                                 // вызов функции на рандом элементов массива (меняем местами)
+        console.log(this.ArrayAnswer);
+        for (let i = 0; i <  this.ArrayAnswer.length; i++) {                  // выводим варианты ответов
+           console.log(this.ArrayAnswer[i] );
+           document.getElementById("answers").innerHTML += "<div class='col'><button class='but'>"+ this.ArrayAnswer[i] + "</button></div>";
+         }
+
+    }
+}
+module.exports = Test ;
