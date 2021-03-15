e17c39bcab794890407667a981670de22270e733
AnigalHub <anigal1997@gmail.com>
2021-03-15 21:57:57 +0300

    добавлено условие если при остановке получается -1

--- TimeCounter.js
+++ TimeCounter.js
@@ -1,30 +1,30 @@
 class TimeСounter { //Класс - Счетчик времени (Таймер)
     constructor(defaultCount) {
-        this.count = defaultCount;// цифра от которой начинаем отсчет таймера
+       // цифра от которой начинаем отсчет таймера
         this.currentInterval = null; // номер текущего таймера
     }
 
-    StartTimeСounter() {  //метод запуска счетчика
+    StartTimeСounter(defaultCount) {  //метод запуска счетчика
+        this.count = defaultCount;
+
         if (this.currentInterval == null) {
             this.currentInterval = setInterval(() => {
-                if (this.count > 0) {
-                    document.getElementById("times").innerHTML = this.count--;
-                    if (this.count < 5) {
-                        document.getElementById("times").style.color = "red";
-                    }
-                    if (this.count == 1){
-                        document.getElementById('playing_field').style.display = "none";
-                    }
+                if (this.count >= 0) {
+                    document.getElementById("timer").innerHTML =  this.count--;
                 }
-            }, 800)
-           // console.log('Запущен интервал под номером ' + this.currentInterval);
+            }, 1000)
+           console.log('Запущен интервал под номером ' + this.currentInterval);
         }
     }
     StopTimeСounter(){ // метод остановки счетчика
         clearInterval(this.currentInterval); // метод останова счетчика
         this.currentInterval = null; // счетчик становится пустым
-        document.getElementById("times").innerHTML = this.count;
+        document.getElementById("timer").innerHTML = this.count;
+        if (this.count == -1){
+            document.getElementById("timer").innerHTML = 0;
+        }
         return this.count;
+
     }
 }
 module.exports = TimeСounter ;
