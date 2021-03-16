7f89466150044ae9c044b6b9d9c8e4d0e9fc5e3a
AnigalHub <anigal1997@gmail.com>
2021-03-16 23:29:59 +0300

    добавлены переменные, при завершении счетчика (-1) - вывод сообщения

--- TimeCounter.js
+++ TimeCounter.js
@@ -1,3 +1,6 @@
+let timer = document.getElementById("timer");
+let modal_window = document.getElementById("myModal2"); // Модальное окно "Игра приостановлена"
+
 class TimeСounter { //Класс - Счетчик времени (Таймер)
     constructor(defaultCount) {
        // цифра от которой начинаем отсчет таймера
@@ -10,21 +13,25 @@ class TimeСounter { //Класс - Счетчик времени (Таймер)
         if (this.currentInterval == null) {
             this.currentInterval = setInterval(() => {
                 if (this.count >= 0) {
-                    document.getElementById("timer").innerHTML =  this.count--;
+                    timer.innerHTML =  this.count--;
+                    if(this.count == -1){
+                        modal_window.style.display = "block"; // выводим сообщение
+                    }
                 }
             }, 1000)
-           console.log('Запущен интервал под номером ' + this.currentInterval);
+           //console.log('Запущен интервал под номером ' + this.currentInterval);
         }
     }
     StopTimeСounter(){ // метод остановки счетчика
-        clearInterval(this.currentInterval); // метод останова счетчика
+        clearInterval(this.currentInterval); // остановка счетчика
         this.currentInterval = null; // счетчик становится пустым
-        document.getElementById("timer").innerHTML = this.count;
+        timer.innerHTML = this.count;
         if (this.count == -1){
-            document.getElementById("timer").innerHTML = 0;
+            timer.innerHTML = 0;
         }
         return this.count;
-
     }
+
+
 }
 module.exports = TimeСounter ;
